26a20f3941553d02e5c9aeb48bfef9fed76acd1f
Alex Martinez <j.alexandermartinez@Alexs-MacBook-Air.local>
2022-02-09 17:26:55 -0500

    more css/html pracitce. with javaScript funtions

---
+++ script.js
@@ -0,0 +1,27 @@
+
+     //we could write javascript within the scrpit
+     document.getElementById('second-heading').innerHTML='Hey Class'
+     
+     
+     const getDataButton = document.getElementById('get-data')
+     getDataButton.addEventListener('click', () => {
+         console.log('testing here')
+
+         function displayData(allbeers){
+             document.querySelector('pre').innerHTML = JSON.stringify
+            (allbeers, null, 2)
+         }
+     
+        fetch('https://api.sampleapis.com/beers/ale')
+        .then(data => data.json())     ///data converted to Json
+        .then(beers => displayData(beers))
+        .catch(err => console.log(err))
+    })
+
+    function showAlert(){
+        alert('hey class we are done!!')
+    }
+
+    const alertBtn = document.getElementById('show-alert-btn')
+    alertBtn.addEventListener('click', showAlert)
+
